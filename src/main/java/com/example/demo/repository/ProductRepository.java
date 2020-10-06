e4471466426eb95989ff600a5aab92d7b997f8db
hfeng <hfeng@fulgentgenetics.com>
2020-10-06 02:55:19 -0700

    removed unused code

--- src/main/java/com/example/demo/repository/ProductRepository.java
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -2,7 +2,6 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Product;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 @Repository
